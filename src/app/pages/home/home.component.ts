a5141e6b57b01f2bbc6b4619aa60549cad0975ed
Thiago Alves <thiagogba@gmail.com>
2024-11-11 19:41:26 -0400

    fat: updated home and content for dynamic data

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -5,6 +5,6 @@ import { Component } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
 
+export class HomeComponent {
 }
